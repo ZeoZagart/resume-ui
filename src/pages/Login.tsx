dcbe0a3930b9df5b467c2638cf4b314dba1d60c9
abhash yadav <abhash.yadav@coinbase.com>
2023-04-02 22:24:34 +0530

    implement apis

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,22 @@
 import { Box, TextField, Button, Typography } from '@mui/material';
+import { useState } from 'react';
+import { useAuth } from '../context/AuthContext';
+import { login } from '../api/resume_service';
 
 const Login: React.FC = () => {
-  const handleLogin = (e: React.FormEvent) => {
-    e.preventDefault();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { setLoggedIn } = useAuth();
 
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const { token } = await login(email, password);
+      localStorage.setItem('token', token);
+      setLoggedIn(true);
+    } catch (error) {
+      // Handle errors
+    }
   };
 
   return (
@@ -18,8 +31,20 @@ const Login: React.FC = () => {
       <Typography variant="h4" gutterBottom>
         Log In
       </Typography>
-      <TextField label="Email" type="email" margin="normal" />
-      <TextField label="Password" type="password" margin="normal" />
+      <TextField
+        label="Email"
+        type="email"
+        margin="normal"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        label="Password"
+        type="password"
+        margin="normal"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <Button type="submit" variant="contained" color="primary">
         Log In
       </Button>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,9 +1,23 @@
 import { Box, TextField, Button, Typography } from '@mui/material';
+import { useState } from 'react';
+import { useAuth } from '../context/AuthContext';
+import { signup } from '../api/resume_service';
 
 const SignUp: React.FC = () => {
-  const handleSubmit = (e: React.FormEvent) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { setLoggedIn } = useAuth();
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    // Perform sign up and API call
+    try {
+      const { token } = await signup(name, email, password);
+      localStorage.setItem('token', token);
+      setLoggedIn(true);
+    } catch (error) {
+      // Handle errors
+    }
   };
 
   return (
@@ -18,9 +32,26 @@ const SignUp: React.FC = () => {
       <Typography variant="h4" gutterBottom>
         Sign Up
       </Typography>
-      <TextField label="Name" margin="normal" />
-      <TextField label="Email" type="email" margin="normal" />
-      <TextField label="Password" type="password" margin="normal" />
+      <TextField
+        label="Name"
+        margin="normal"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <TextField
+        label="Email"
+        type="email"
+        margin="normal"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        label="Password"
+        type="password"
+        margin="normal"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <Button type="submit" variant="contained" color="primary">
         Sign Up
       </Button>
