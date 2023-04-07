3c2fae54c5d936829d633063fe9cb655697a7445
ZeoZagart <abhashasd1@gmail.com>
2023-04-07 22:35:59 +0530

    Add generate coverletter and upload resumes page

---
+++ src/components/UploadResume.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
+import { uploadResume } from '../api/resume_service';
+import { useAuth } from '../context/AuthContext';
+import { Resume } from '../api/types';
+
+interface UploadResumesProps {
+    open: boolean;
+    onClose: () => void;
+    onUploadSuccess: (uploadedResume: Resume) => void;
+    setError: (error: string | null) => void;
+}
+
+const UploadResumes: React.FC<UploadResumesProps> = ({ open, onClose, onUploadSuccess, setError }) => {
+    const { token } = useAuth();
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const [metadata, setMetadata] = useState('');
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedFile(event.target.files?.[0] || null);
+    };
+
+    const handleMetadataChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setMetadata(event.target.value);
+    };
+
+    const handleUploadResume = async () => {
+        if (selectedFile) {
+            try {
+                const metadataObject = metadata ? JSON.parse(metadata) : {};
+                const response = await uploadResume(token!!, metadataObject, selectedFile);
+
+                if (response.state === 'SUCCESS') {
+                    onUploadSuccess(response.data);
+                    setSelectedFile(null);
+                    setMetadata('');
+                    onClose();
+                } else {
+                    setError(`Error uploading resume: ${response.error}`);
+                }
+            } catch (error) {
+                setError('Invalid metadata JSON format. Please correct and try again.');
+            }
+        } else {
+            setError('Please select a file to upload.');
+        }
+    };
+
+    return (
+        <Dialog open={open} onClose={onClose}>
+            <DialogTitle>Upload New Resume</DialogTitle>
+            <DialogContent>
+                <input
+                    accept="application/pdf"
+                    style={{ display: 'none' }}
+                    id="upload-file"
+                    type="file"
+                    onChange={handleFileChange}
+                    aria-label="Select resume file"
+                />
+                <label htmlFor="upload-file">
+                    <Button variant="outlined" color="primary" component="span">
+                        {selectedFile ? selectedFile.name : 'Choose File'}
+                    </Button>
+                </label>
+                <TextField
+                    label="Metadata (Optional, JSON format)"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    value={metadata}
+                    onChange={handleMetadataChange}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={onClose} color="secondary">
+                    Cancel
+                </Button>
+                <Button onClick={handleUploadResume} color="primary">
+                    Upload
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default UploadResumes;
--- src/pages/dashboard/GenerateCoverLetter.tsx
+++ src/pages/dashboard/GenerateCoverLetter.tsx
@@ -1,7 +1,124 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {
+    Button,
+    TextField,
+    Box,
+    Typography,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+} from '@mui/material'
+import { generateCoverLetter, listResumes } from '../../api/resume_service'
+import { useAuth } from '../../context/AuthContext'
+import { Resume } from '../../api/types'
 
-const GenerateCoverLetter: React.FC = () => {
-    return <div>Generate Cover Letter Content</div>
+const GenerateCoverLetter = () => {
+    const { token } = useAuth()
+    const [jobDescription, setJobDescription] = useState('')
+    const [generatedCoverLetter, setGeneratedCoverLetter] = useState('')
+    const [selectedResumeId, setSelectedResumeId] = useState('')
+    const [resumes, setResumes] = useState<Resume[]>([])
+
+    useEffect(() => {
+        const fetchResumes = async () => {
+            const response = await listResumes(token!!)
+            if (response.state === 'SUCCESS') {
+                setResumes(response.data.resumes)
+            } else {
+                console.error('Error fetching resumes:', response.error)
+            }
+        }
+
+        fetchResumes()
+    }, [token])
+
+    const handleSubmit = async () => {
+        const response = await generateCoverLetter(token!!, {
+            resume_id: selectedResumeId,
+            job_desc: jobDescription,
+        })
+
+        if (response.state === 'SUCCESS') {
+            setGeneratedCoverLetter(response.data)
+        } else {
+            console.error('Error generating cover letter:', response.error)
+        }
+    }
+
+    return (
+        <Box>
+            <Typography variant="h4">Generate Cover Letter</Typography>
+            <Box
+                component="form"
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    mt: 3,
+                }}
+            >
+                <FormControl sx={{ minWidth: 200, mt: 2 }}>
+                    <InputLabel id="resume-select-label">
+                        Select Resume
+                    </InputLabel>
+                    <Select
+                        labelId="resume-select-label"
+                        id="resume-select"
+                        value={selectedResumeId}
+                        label="Select Resume"
+                        onChange={(e) =>
+                            setSelectedResumeId(e.target.value as string)
+                        }
+                    >
+                        {resumes.map((resume) => (
+                            <MenuItem key={resume.id} value={resume.id}>
+                                {resume.file_name}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <TextField
+                    label="Job Description"
+                    variant="outlined"
+                    multiline
+                    rows={6}
+                    value={jobDescription}
+                    onChange={(e) => setJobDescription(e.target.value)}
+                    sx={{ minWidth: 400, mt: 2 }}
+                />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSubmit}
+                    sx={{ mt: 2 }}
+                    disabled={!selectedResumeId}
+                >
+                    Generate
+                </Button>
+            </Box>
+            {generatedCoverLetter && (
+                <Box sx={{ mt: 4 }}>
+                    <Typography variant="h6">
+                        Generated Cover Letter:
+                    </Typography>
+                    <Box
+                        component="pre"
+                        sx={{
+                            background: (theme) =>
+                                theme.palette.background.default,
+                            borderRadius: 1,
+                            p: 2,
+                            mt: 2,
+                            whiteSpace: 'pre-wrap',
+                        }}
+                    >
+                        {generatedCoverLetter}
+                    </Box>
+                </Box>
+            )}
+        </Box>
+    )
 }
 
 export default GenerateCoverLetter
