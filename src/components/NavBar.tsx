d79372bad091c84fc94d2972237024020d5fca6e
ZeoZagart <abhashasd1@gmail.com>
2023-04-09 23:31:08 +0530

    improve theme

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,57 @@
-import { AppBar, Toolbar, Typography, Button } from '@mui/material'
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Button,
+    IconButton,
+    Avatar,
+    Menu,
+    MenuItem,
+} from '@mui/material'
 import { useAuth } from '../context/AuthContext'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const NavBar: React.FC = () => {
     const { isLoggedIn, setToken } = useAuth()
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+    const handleMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
 
     const logOutButtons = () => {
         return (
-            <Button color="inherit" onClick={() => setToken(null)}>
-                Logout
-            </Button>
+            <>
+                <IconButton
+                    edge="end"
+                    color="secondary"
+                    aria-label="account of current user"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                >
+                    <Avatar>
+                        {"A"}
+                    </Avatar>
+                </IconButton>
+                <Menu
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose} component={Link} to="/account-settings">
+                        Account Settings
+                    </MenuItem>
+                    <MenuItem onClick={handleClose} component={Link} to="/subscription">
+                        Subscription
+                    </MenuItem>
+                    <MenuItem onClick={() => setToken(null)}>Logout</MenuItem>
+                </Menu>
+            </>
         )
     }
 
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -6,14 +6,14 @@ import {
     List,
     Divider,
     ListItemButton,
+    Card,
+    CardContent,
 } from '@mui/material'
 import MyResumes from './MyResumes'
 import GenerateCoverLetter from './GenerateCoverLetter'
-import AccountSettings from './AccountSettings'
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import DescriptionIcon from '@mui/icons-material/Description'
-import SettingsIcon from '@mui/icons-material/Settings'
 import PostAddIcon from '@mui/icons-material/PostAdd'
 
 const Dashboard: React.FC = () => {
@@ -38,54 +38,48 @@ const Dashboard: React.FC = () => {
                     >
                         <ListItemIcon>
                             {drawerOpen ? (
-                                <ChevronLeftIcon />
-                            ) : (
-                                <ChevronRightIcon />
-                            )}
-                        </ListItemIcon>
-                        {drawerOpen && <ListItemText primary="Hide" />}
-                    </ListItemButton>
-                    <Divider />
-                    <ListItemButton
-                        onClick={() => handleChange(0)}
-                        sx={{ height: '48px' }}
-                    >
-                        <ListItemIcon>
-                            <DescriptionIcon />
-                        </ListItemIcon>
-                        {drawerOpen && <ListItemText primary="My Resumes" />}
-                    </ListItemButton>
-                    <ListItemButton
-                        onClick={() => handleChange(1)}
-                        sx={{ height: '48px' }}
-                    >
-                        <ListItemIcon>
-                            <PostAddIcon />
-                        </ListItemIcon>
-                        {drawerOpen && (
-                            <ListItemText primary="Generate Cover Letter" />
-                        )}
-                    </ListItemButton>
-                    <ListItemButton
-                        onClick={() => handleChange(2)}
-                        sx={{ height: '48px' }}
-                    >
-                        <ListItemIcon>
-                            <SettingsIcon />
-                        </ListItemIcon>
-                        {drawerOpen && (
-                            <ListItemText primary="Account Settings" />
-                        )}
-                    </ListItemButton>
-                </List>
-            </Box>
-            <Box p={2} flexGrow={1}>
-                {tabValue === 0 && <MyResumes />}
-                {tabValue === 1 && <GenerateCoverLetter />}
-                {tabValue === 2 && <AccountSettings />}
-            </Box>
-        </Box>
-    )
-}
-
-export default Dashboard
+                               
+                               <ChevronLeftIcon />
+                               ) : (
+                                   <ChevronRightIcon />
+                               )}
+                           </ListItemIcon>
+                           {drawerOpen && <ListItemText primary="Hide" />}
+                       </ListItemButton>
+                       <Divider />
+                       <ListItemButton
+                           onClick={() => handleChange(0)}
+                           sx={{ height: '48px' }}
+                       >
+                           <ListItemIcon>
+                               <DescriptionIcon />
+                           </ListItemIcon>
+                           {drawerOpen && <ListItemText primary="My Resumes" />}
+                       </ListItemButton>
+                       <ListItemButton
+                           onClick={() => handleChange(1)}
+                           sx={{ height: '48px' }}
+                       >
+                           <ListItemIcon>
+                               <PostAddIcon />
+                           </ListItemIcon>
+                           {drawerOpen && (
+                               <ListItemText primary="Generate Cover Letter" />
+                           )}
+                       </ListItemButton>
+                   </List>
+               </Box>
+               <Box p={2} flexGrow={1}>
+                   <Card>
+                       <CardContent>
+                           {tabValue === 0 && <MyResumes />}
+                           {tabValue === 1 && <GenerateCoverLetter />}
+                       </CardContent>
+                   </Card>
+               </Box>
+           </Box>
+       )
+   }
+   
+   export default Dashboard
+   
